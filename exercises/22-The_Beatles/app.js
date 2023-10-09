e83ca68fbef97f181820c8ed0d33384ae5fc08ea
McMartinezc <psyence_mc@hotmail.com>
2023-10-09 13:50:40 +0200

    falta 23

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -2,4 +2,15 @@
 
 //Your code above ^^^
 
+function sing(){
+    let frase = "";
+
+    for (let i=0; i<11; i++){
+        if(i===4) frase += 'there will be an answer, ';
+        else if(i===10) frase += 'whisper words of wisdom ';
+        else frase += 'let it be, ';
+    }
+    return frase;
+}
+
 console.log(sing());
